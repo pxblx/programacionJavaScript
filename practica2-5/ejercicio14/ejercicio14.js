3abc9dfa7c058d93b14912a3305db40bcdb85e63
pxblx <pablomurilloavila@gmail.com>
2019-10-26 18:27:56 +0200

    Corregir Ejercicio 14 (fixes #1)

--- practica2-5/ejercicio14/ejercicio14.js
+++ practica2-5/ejercicio14/ejercicio14.js
@@ -3,22 +3,23 @@
  * 
  * @author Pablo
  */
-
-let muestraOculta = function(id) {
-    let elemento = document.getElementById("contenidos_" + id);
-    let enlace = document.getElementById("enlace_" + id);
-    
-    if (elemento.style.display == "" || elemento.style.display == "block") {
-        elemento.style.display = "none";
-        enlace.innerHTML = "Mostrar contenidos";
-    } else {
-        elemento.style.display = "block";
-        enlace.innerHTML = "Ocultar contenidos";
+{
+    let muestraOculta = function (id) {
+        let contenidos = document.getElementById("contenidos_" + id);
+        if (contenidos.style.display == "" || contenidos.style.display == "block") {
+            contenidos.style.display = "none";
+            this.innerHTML = "Mostrar contenidos";
+        } else {
+            contenidos.style.display = "block";
+            this.innerHTML = "Ocultar contenidos";
+        }
     }
-}
 
-window.onload = function() {
-    document.getElementById("enlace_1").addEventListener("click", function(){muestraOculta("1")});
-    document.getElementById("enlace_2").addEventListener("click", function(){muestraOculta("2")});
-    document.getElementById("enlace_3").addEventListener("click", function(){muestraOculta("3")});
+    let init = function () {
+        document.getElementById("enlace_1").addEventListener("click", function () { muestraOculta("1") });
+        document.getElementById("enlace_2").addEventListener("click", function () { muestraOculta("2") });
+        document.getElementById("enlace_3").addEventListener("click", function () { muestraOculta("3") });
+    }
+
+    window.addEventListener("DOMContentLoaded", init);
 }
