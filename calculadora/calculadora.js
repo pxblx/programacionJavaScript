938a9c629af4fbd755183adeb0eaa102c8ac484e
pxblx <pablomurilloavila@gmail.com>
2019-11-27 12:46:17 +0100

    Ejercicio calculadora

---
+++ calculadora/calculadora.js
@@ -0,0 +1,45 @@
+/**
+ * Calculadora
+ * 
+ * @author Pablo
+ */
+
+{
+    let init = function () {
+        let div = document.createElement("div");
+        div.style.border = "1px solid";
+        div.style.display = "inline-block";
+        div.style.padding = "10px";
+
+        let display = document.createElement("input");
+        display.setAttribute("type", "text");
+        display.style.marginBottom = "10px";
+        display.style.width = "100%";
+        display.style.boxSizing = "border-box";
+        div.appendChild(display);
+        div.appendChild(document.createElement("br"));
+
+        let botones = ["CE", "<-", "%", "+",
+            "7", "8", "9", "-",
+            "4", "5", "6", "x",
+            "1", "2", "3", "/",
+            "0", "+/-", ".", "="];
+        let counter = 1;
+        botones.forEach(texto => {
+            let boton = document.createElement("button");
+            boton.style.width = "50px";
+            boton.style.height = "30px";
+            boton.innerHTML = texto;
+            div.appendChild(boton);
+            if (counter == 4) {
+                div.appendChild(document.createElement("br"));
+                counter = 0;
+            }
+            counter++;
+        });
+
+        document.body.appendChild(div);
+    }
+    
+    addEventListener("DOMContentLoaded", init);
+}
