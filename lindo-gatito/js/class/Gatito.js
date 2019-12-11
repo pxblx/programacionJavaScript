56770c49fb58aff0281b6ebfaa200c7da08fc0a5
pxblx <pablomurilloavila@gmail.com>
2019-12-11 12:06:41 +0100

    Clase Gatito usando prototype

--- lindo-gatito/js/class/Gatito.js
+++ lindo-gatito/js/class/Gatito.js
@@ -5,48 +5,46 @@
  * @author Pablo
  */
 
-class Gatito {
-    /**
-     * Constructor
-     * 
-     * @param {String} nombre Nombre del gatito
-     * @param {Date} nacimiento Fecha de nacimiento del gatito
-     * @param {String} raza Raza del gatito
-     * @param {Number} peso Peso del gatito
-     * @param {String} estado Estado del gatito
-     */
-    constructor(nombre, nacimiento, raza, peso, estado) {
-        this.nombre = nombre;
-        this.nacimiento = nacimiento;
-        this.raza = raza;
-        this.peso = peso;
-        this.estado = estado;
-    }
+/**
+ * Gatito
+ * 
+ * @param {String} nombre Nombre del gatito
+ * @param {Date} nacimiento Fecha de nacimiento del gatito
+ * @param {String} raza Raza del gatito
+ * @param {Number} peso Peso del gatito
+ * @param {String} estado Estado del gatito
+ */
+function Gatito(nombre, nacimiento, raza, peso, estado) {
+    this.nombre = nombre;
+    this.nacimiento = nacimiento;
+    this.raza = raza;
+    this.peso = peso;
+    this.estado = estado;
+}
 
-    /**
-     * Devolver si el gatito está vivo o no
-     * 
-     * @returns true o false
-     */
-    estaVivo () {
-        return this.peso > 0 && this.peso < 10;
-    }
+/**
+ * Devolver si el gatito está vivo o no
+ * 
+ * @returns true o false
+ */
+Gatito.prototype.estaVivo = function () {
+    return this.peso > 0 && this.peso < 10;
+}
 
-    /**
-     * Jugar (decrementar peso)
-     */
-    jugar () {
-        if (this.estaVivo) {
-            this.peso--;
-        }
+/**
+ * Jugar (decrementar peso)
+ */
+Gatito.prototype.jugar = function () {
+    if (this.estaVivo) {
+        this.peso--;
     }
+}
 
-    /**
-     * Comer (incrementar peso)
-     */
-    comer () {
-        if (this.estaVivo) {
-            this.peso++;
-        }
+/**
+ * Comer (incrementar peso)
+ */
+Gatito.prototype.comer = function () {
+    if (this.estaVivo) {
+        this.peso++;
     }
 }
